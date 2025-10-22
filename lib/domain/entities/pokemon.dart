605c1a09f58083f9f10dc33f2fc289f5727a25a5
Motebang Mokwatsi <motebangmokwatsi0@gmail.com>
2025-10-22 23:21:01 +0200

    task: create a pokemon entity

---
+++ lib/domain/entities/pokemon.dart
@@ -0,0 +1,19 @@
+class Pokemon {
+  final int id;
+  final String name;
+  final String imageUrl;
+  final List<String> types;
+  final int height;
+  final int weight;
+  final Map<String, int> stats;
+
+  const Pokemon({
+    required this.id,
+    required this.name,
+    required this.imageUrl,
+    required this.types,
+    required this.height,
+    required this.weight,
+    required this.stats,
+  });
+}
