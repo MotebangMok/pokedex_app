8f6c6171c2d2e14c20b65796b4443905fba6d243
Motebang Mokwatsi <motebangmokwatsi0@gmail.com>
2025-10-22 22:40:19 +0200

    chore: declare api constants

---
+++ lib/core/constants/api_constants.dart
@@ -0,0 +1,8 @@
+class ApiConstants {
+  static const String baseUrl = 'https://pokeapi.co/api/v2';
+  static const String pokemonEndpoint = '/pokemon';
+  static const String pokemonSpeciesEndpoint = '/pokemon-species';
+
+  static const int defaultLimit = 30;
+  static const int defaultOffset = 0;
+}
